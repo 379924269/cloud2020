66f4a8fffd8f5453ac9f7f6de2b68be1a07c4aeb
huazai <379924269@qq.com>
2022-07-19 23:53:19 +0800

    建模块

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/spirngcloud/CloudProviderPayment8001.java
@@ -0,0 +1,12 @@
+package com.atguigu.spirngcloud;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudProviderPayment8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudProviderPayment8001.class, args);
+    }
+}
