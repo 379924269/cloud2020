575b8999b2d29caf98c14e16e386a5f770719f61
huazai <379924269@qq.com>
2022-07-20 23:50:35 +0800

    服务发现

--- cloud-provider-payment8003/src/main/java/com/atguigu/spirngcloud/CloudProviderPayment8003.java
+++ cloud-provider-payment8003/src/main/java/com/atguigu/spirngcloud/CloudProviderPayment8003.java
@@ -3,10 +3,12 @@ package com.atguigu.spirngcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class CloudProviderPayment8003 {
     public static void main(String[] args) {
         SpringApplication.run(CloudProviderPayment8003.class, args);
--- cloud-provider-payment8003/src/main/java/com/atguigu/spirngcloud/controller/PaymentController.java
+++ cloud-provider-payment8003/src/main/java/com/atguigu/spirngcloud/controller/PaymentController.java
@@ -3,16 +3,21 @@ package com.atguigu.spirngcloud.controller;
 import com.atguigu.spirngcloud.entities.CommonResult;
 import com.atguigu.spirngcloud.entities.Payment;
 import com.atguigu.spirngcloud.service.PaymentService;
+import com.sun.xml.internal.fastinfoset.util.ValueArray;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import javax.sound.sampled.Port;
-import javax.websocket.server.PathParam;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author huazai
@@ -24,6 +29,9 @@ import javax.websocket.server.PathParam;
 public class PaymentController {
     @Resource
     private PaymentService paymentService;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @Value("${server.port}")
     private String port;
@@ -38,5 +46,17 @@ public class PaymentController {
     public CommonResult get(@PathVariable("id") Integer id) {
         final Payment payment = paymentService.getPaymentById(id);
         return new CommonResult(200, "成功 :" + port, payment);
+    }
+
+    @GetMapping("/payment/getService")
+    public Object getService() {
+        List<String> services = discoveryClient.getServices();
+        List<ServiceInstance> allList = new ArrayList<>();
+        for (String service : services) {
+            log.info("service : {}", service);
+            List<ServiceInstance> instances = discoveryClient.getInstances(service);
+            allList.addAll(instances);
+        }
+        return allList;
     }
 }
